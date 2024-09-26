fd8fe2280b1a85b211f0ce0ae779f0cf16447359
saumittras <saumittra.eee@gmail.com>
2024-09-27 02:04:20 +0600

    added filter, find, forEach, and reduce

---
+++ filter.js
@@ -0,0 +1,14 @@
+// filter selects elements based on a condition and returs an array  with the elements that fulfilled the condition
+const numbers = [1, 2, 5, 6, 4, 15];
+const player = [75, 65, 67, 72, 55, 59];
+const friends = ["Tom", "John", "Micleal", "Oliver", "Tim", "Joshna"];
+
+// cwill return
+// const selected = player.filter((p) => p > 70);
+const selected = player.filter((p) => p % 2 == 1);
+console.log(selected);
+
+// friend
+
+const oddfriends = friends.filter((friend) => friend.length > 4);
+console.log(oddfriends);
---
+++ find.js
@@ -0,0 +1,5 @@
+const numbers = [1, 2, 5, 6, 4, 15];
+const player = [75, 65, 67, 72, 55, 59];
+const friends = ["Tom", "John", "Micleal", "Oliver", "Tim", "Joshna"];
+const selected = player.find((num) => num > 60);
+console.log(selected);
